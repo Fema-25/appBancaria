5d464a77c9ea1e2db7f2834b4bd342a051d29375
Francisco <francisconu1125@gmail.com>
2023-07-26 11:10:50 -0300

    bases del proyecto

---
+++ src/main/java/Aceleracion/AppBancaria/AppBancariaApplication.java
@@ -0,0 +1,13 @@
+package Aceleracion.AppBancaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBancariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBancariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Aceleracion/AppBancaria/AppBancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package Aceleracion.AppBancaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBancariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
