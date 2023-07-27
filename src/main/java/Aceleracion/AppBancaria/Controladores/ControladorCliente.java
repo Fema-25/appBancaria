dfaa8cd99c08c96511bb24f52aa6c202e632f906
Francisco <francisconu1125@gmail.com>
2023-07-26 23:24:14 -0300

    inyectando ServicioCliente a ControladorCliente

--- src/main/java/Aceleracion/AppBancaria/Controladores/ControladorCliente.java
+++ src/main/java/Aceleracion/AppBancaria/Controladores/ControladorCliente.java
@@ -1,9 +1,16 @@
 package Aceleracion.AppBancaria.Controladores;
 
 import Aceleracion.AppBancaria.Servicios.ServicioCliente;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/cliente")
 public class ControladorCliente {
+    private final ServicioCliente servCliente;
+
+    public ControladorCliente(ServicioCliente servCliente){
+        this.servCliente = servCliente;
+    }
     
 }
